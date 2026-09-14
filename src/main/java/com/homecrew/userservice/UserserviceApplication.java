1f19942ad34aed640e37ad26a4e4e6b0938cd4cf
mThanuj <mullagrithanuj0@gmailcom>
2026-09-14 14:38:10 +0530

    feat: register user service with eureka and config server

--- src/main/java/com/homecrew/userservice/UserserviceApplication.java
+++ src/main/java/com/homecrew/userservice/UserserviceApplication.java
@@ -2,12 +2,12 @@ package com.homecrew.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class UserserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(UserserviceApplication.class, args);
+  }
 }
